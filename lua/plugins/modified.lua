1cd7425c25dbeff72f6b84a7112806ba90554912
February Cozzocrea <february.cozzocrea@gmail.com>
2024-08-25 17:24:00 -0700

    change lualine config. change dashboard config.

--- lua/plugins/modified.lua
+++ lua/plugins/modified.lua
@@ -2,14 +2,110 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = {
+      options = {
+        section_separators = { left = "", right = "█" },
+      },
       sections = {
+        lualine_x = {
+          -- NOTE: This is in the default config, but it's a bit noisy.
+          --
+          -- stylua: ignore
+          --{
+          --  function() return require("noice").api.status.command.get() end,
+          --  cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+          --  color = function() return LazyVim.ui.fg("Statement") end,
+          --},
+          -- stylua: ignore
+          {
+            function() return require("noice").api.status.mode.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+            color = function() return LazyVim.ui.fg("Constant") end,
+          },
+          -- stylua: ignore
+          {
+            function() return "  " .. require("dap").status() end,
+            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+            color = function() return LazyVim.ui.fg("Debug") end,
+          },
+          -- stylua: ignore
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = function() return LazyVim.ui.fg("Special") end,
+          },
+        },
         lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 1 } },
+          { "progress", separator = " ", padding = { left = 0, right = 1 } },
         },
         lualine_z = {
-          { "location", padding = { left = 1, right = 1 } },
+          { "location", padding = { left = 0, right = 1 } },
         },
       },
     },
+  },
+  {
+    "nvimdev/dashboard-nvim",
+    lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
+    opts = function()
+      local logo = [[
+         ███╗  ██╗ ██████╗  █████╗  ██╗   ██╗ ██╗ ███╗   ███╗           
+         ████╗ ██║ ██════╝ ██   ██╗ ██║   ██║ ██║ ████╗ ████║           
+         ██╔██╗██║ ██████╗ ██   ██║ ██║   ██║ ██║ ██╔████╔██║           
+         ██║╚████║ ██════╝ ██   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║           
+         ██║ ╚███║ ██████╗ ╚█████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║           
+         ╚═╝   ╚═╝ ╚═════╝  ╚════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝           
+    ]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+      local opts = {
+        theme = "doom",
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, "\n"),
+        -- stylua: ignore
+        center = {
+          { action = 'lua LazyVim.pick()()',                           desc = " Find File",       icon = " ", key = "f" },
+          { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
+          { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
+          { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
+          { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
+          { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
+          { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
+          { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
+          { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
+        },
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
+      }
+
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
+      end
+
+      -- open dashboard after closing lazy
+      if vim.o.filetype == "lazy" then
+        vim.api.nvim_create_autocmd("WinClosed", {
+          pattern = tostring(vim.api.nvim_get_current_win()),
+          once = true,
+          callback = function()
+            vim.schedule(function()
+              vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
+            end)
+          end,
+        })
+      end
+
+      return opts
+    end,
   },
 }
