0a5bf03d48478a8ffb20a34ce56db758cc4c6131
february cozzocrea <february.cozzocrea@gmail.com>
2024-08-26 11:22:24 -0700

    add options.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.have_nerd_font = true
+
+local opt = vim.opt
+
+-- Don't show the keypresses in the bottom line of code.
+opt.showcmd = false
+
+-- Turn off relative line numbers.
+opt.relativenumber = false
